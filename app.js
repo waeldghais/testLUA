df45dcaee6e481ac925237d686ca71fe0d68677c
Wael Dghais <wael@mdw.local>
2021-09-28 17:02:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+ 
+
+
+ function showborder(){
+    $(event.target).toggleClass("mystyle");
+ }
+
+// function showborder(){
+//     document.body.addEventListener('mouseover',function(e){
+//     let tagName = e.target.nodeName;
+//     tagName.classList.toggle("mystyle")
+//    //console.log(tagName)
+// });
+// }
+
+
+    //var x =document.getElementsByTagName(tag);
+    // console.log(tag);
+    
+    // console.log(tag.classList.value);
+    // if(tag.classList.value=="mystyle"){
+    //     tag.classList.toggle("nullstyle");
+    //     console.log(tag);
+    // }else{
+    //     tag.classList.add("mystyle");
+    //     console.log(tag);
+    // }
+    // if(tag.style.border=="2px solid green"){
+    //     tag.style.border="";
+    // }else{
+    //     tag.style.border="2px solid green"
+    // }
